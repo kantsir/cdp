7856ec7da480f70a1573fea2acab86fb7417b5b6
kantsir <kantsir@krom>
2012-02-04 15:31:01 +0200

    unitils tests properties

--- src/main/java/com/epam/cdp/mbank/App.java
+++ src/main/java/com/epam/cdp/mbank/App.java
@@ -1,16 +1,12 @@
 package com.epam.cdp.mbank;
 
-import com.epam.cdp.mbank.core.db.dao.ClientDao;
-import com.epam.cdp.mbank.model.Client;
-import com.epam.cdp.mbank.model.enums.ClientActiveState;
-import com.epam.cdp.mbank.model.enums.ClientType;
 
 public class App {
 
     public static void main(String[] args) {
 	System.out.println("Hello World!");
 
-	 Client client = new Client();
+	/* Client client = new Client();
 	 client.setClientName("Roman");
 	 client.setComment("TEST Comments");
 	 client.setEmail("kantsir.roman@gmail.com");
@@ -19,7 +15,7 @@ public class App {
 	 
 	 ClientDao cd = new ClientDao();
 	 cd.save(client);
-	
+	*/
 	
 	
 	
--- src/test/java/com/epam/cdp/mbank/ClientDaoTest.java
+++ src/test/java/com/epam/cdp/mbank/ClientDaoTest.java
@@ -1,20 +1,42 @@
 package com.epam.cdp.mbank;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.After;
 import org.junit.Test;
+
 import org.junit.runner.RunWith;
 import org.unitils.UnitilsJUnit4;
 import org.unitils.UnitilsJUnit4TestClassRunner;
-import org.unitils.database.annotations.Transactional;
-import org.unitils.database.util.TransactionMode;
+
 import org.unitils.dbunit.annotation.DataSet;
+import org.unitils.reflectionassert.ReflectionAssert;
+
+import com.epam.cdp.mbank.core.db.dao.ClientDao;
+import com.epam.cdp.mbank.model.Client;
+import com.epam.cdp.mbank.model.enums.ClientActiveState;
 
 @RunWith(UnitilsJUnit4TestClassRunner.class)
 public class ClientDaoTest extends UnitilsJUnit4 {
 
 	@Test
-	@DataSet("ClientDaoTest.findClientByID.xml")
-	@Transactional(value=TransactionMode.ROLLBACK)
-	public void firstTest(){
-		
+	@DataSet("ClientDaoTest.clients.xml")
+	public void firstTest() {
+
+		ClientDao cd = new ClientDao();
+		List<Client> clientsActual = cd
+				.getClientsByActiveState(ClientActiveState.ACTIVE);
+
+		ReflectionAssert.assertPropertyLenientEquals("clientName", Arrays
+				.asList("testDatabaseFieldName1", "testDatabaseFieldName2",
+						"testDatabaseFieldName3", "testDatabaseFieldName4",
+						"testDatabaseFieldName5"), clientsActual);
+
+	}
+
+	@After
+	public void revertLastChangesInDB() {
+		System.out.println("Cleaning...");
 	}
 }
